0a6f1bf110199f6bfa09f7506e415eab32aadc2a
serdar <serdaralkan95@gmail.com>
2023-05-08 08:44:43 -0700

    clean-up

--- pages/index-bu.js
+++ pages/index-bu.js
@@ -1,7 +1,18 @@
 import { useState, useMemo } from "react";
-import { DrawingManager, GoogleMap, useLoadScript, Marker, Polygon, LoadScript } from "@react-google-maps/api";
-import usePlacesAutocomplete, { getGeocode, getLatLng } from "use-places-autocomplete";
-import { Combobox, ComboboxInput, ComboboxPopover, ComboboxList, ComboboxOption } from "@reach/combobox";
+import { DrawingManager, GoogleMap, useLoadScript, Marker, Polygon, LoadScript} from "@react-google-maps/api";
+import usePlacesAutocomplete, {
+  getGeocode,
+  getLatLng,
+} from "use-places-autocomplete";
+
+
+import {
+  Combobox,
+  ComboboxInput,
+  ComboboxPopover,
+  ComboboxList,
+  ComboboxOption,
+} from "@reach/combobox";
 import "@reach/combobox/styles.css";
 import { Draggable } from 'react-draggable';
 
@@ -12,43 +23,49 @@ export default function Places() {
   });
 
   if (!isLoaded) return <div>Loading...</div>;
-
   return <Map />;
 }
 
 function Map() {
-  // Set up the initial map center
   const center = useMemo(() => ({ lat: 49.26016048544659, lng: -123.01855939966318 }), []);
   const [selected, setSelected] = useState(center);
 
-  // Event handlers for DrawingManager
+
+
   const onLoad = drawingManager => {
     console.log(drawingManager)
   }
-
+  
   const onPolygonComplete = polygon => {
     console.log(polygon)
   } 
 
-  // Render the GoogleMap component with a Marker and DrawingManager
   return (
     <>
       <div className="places-container">
         <PlacesAutocomplete setSelected={setSelected} />
       </div>
     
-      <GoogleMap
-        id="drawing-manager-example"
-        mapContainerClassName="map-container"
-        zoom={2.5}
-        zoom={15}
-        center={selected}
-      >
-        {selected && <Marker position={selected} />}
-        
-        <DrawingManager
-          onLoad={onLoad}
-          defaultDrawingMode={google.maps.drawing.OverlayType.POLYGON}
+
+    
+
+
+
+  <GoogleMap
+    id="drawing-manager-example"
+    mapContainerClassName="map-container"
+    zoom={2.5}
+    zoom={15}
+    center={selected}
+
+  >
+    {selected && <Marker position={selected} />}
+    
+    <DrawingManager
+      onLoad={onLoad}
+      //onPolygonComplete={onPolygonComplete}
+
+      defaultDrawingMode={google.maps.drawing.OverlayType.POLYGON}
           defaultOptions={{
             drawingControl: true,
             drawingControlOptions: {
@@ -58,13 +75,42 @@ function Map() {
             polygonOptions: { editable: true, draggable: true }
           }}
           onPolygonComplete={value => getPaths(value)}
-        />
+
+    />
+       
+   
       </GoogleMap>
+
+    {/* <LoadScript
+    
+          id="script-loader"
+          // googleMapsApiKey={apiKey}
+          // libraries={libraries}
+          language="en"
+          region="us"
+    > */}
+
+
+      {/* <GoogleMap
+        zoom={12}
+        center={selected}
+        mapContainerClassName="map-container"
+      >
+        {selected && <Marker position={selected} />}
+
+        <DrawingManager
+      onLoad={onLoad}
+      onPolygonComplete={onPolygonComplete}
+    />
+
+      </GoogleMap>
+    </LoadScript> */}
+
     </>
+
   );
 }
 
-// Component for the PlacesAutocomplete search bar
 const PlacesAutocomplete = ({ setSelected }) => {
   const {
     ready,
