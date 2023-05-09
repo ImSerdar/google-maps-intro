8612012261f42d4f9eb7af3e30e10e40c6e73f2b
serdar <serdaralkan95@gmail.com>
2023-05-09 08:57:45 -0700

    polygon paths

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import { useState, useMemo } from "react";
+import { useState, useMemo, useEffect } from "react";
 import { DrawingManager, GoogleMap, useLoadScript, Marker, Polygon, LoadScript } from "@react-google-maps/api";
 import usePlacesAutocomplete, { getGeocode, getLatLng } from "use-places-autocomplete";
 import { Combobox, ComboboxInput, ComboboxPopover, ComboboxList, ComboboxOption } from "@reach/combobox";
@@ -17,52 +17,78 @@ export default function Places() {
 }
 
 function Map() {
-  // Set up the initial map center
-  const center = useMemo(() => ({ lat: 49.26016048544659, lng: -123.01855939966318 }), []);
-  const [selected, setSelected] = useState(center);
+    // Set up the initial map center
+    const center = useMemo(() => ({ lat: 49.26016048544659, lng: -123.01855939966318 }), []);
+    const [selected, setSelected] = useState(center);
+    const [polygonPaths, setPolygonPaths] = useState([]);
 
-  // Event handlers for DrawingManager
-  const onLoad = drawingManager => {
-    console.log(drawingManager)
-  }
-
-  const onPolygonComplete = polygon => {
-    console.log(polygon)
-  } 
-
-  // Render the GoogleMap component with a Marker and DrawingManager
-  return (
-    <>
-      <div className="places-container">
-        <PlacesAutocomplete setSelected={setSelected} />
-      </div>
     
-      <GoogleMap
-        id="drawing-manager-example"
-        mapContainerClassName="map-container"
-        zoom={2.5}
-        zoom={15}
-        center={selected}
-      >
-        {selected && <Marker position={selected} />}
-        
-        <DrawingManager
-          onLoad={onLoad}
-          defaultDrawingMode={google.maps.drawing.OverlayType.POLYGON}
-          defaultOptions={{
-            drawingControl: true,
-            drawingControlOptions: {
-              position: google.maps.ControlPosition.TOP_CENTER,
-              drawingModes: [google.maps.drawing.OverlayType.POLYGON]
-            },
-            polygonOptions: { editable: true, draggable: true }
-          }}
-          onPolygonComplete={value => getPaths(value)}
-        />
-      </GoogleMap>
-    </>
-  );
-}
+    // Load polygon paths from local storage on component mount
+    useEffect(() => {
+        const savedPaths = JSON.parse(localStorage.getItem('polygonPaths'));
+        if (savedPaths) {
+        setPolygonPaths(savedPaths);
+        }
+    }, []);
+
+    // Save polygon paths to local storage when they change
+    useEffect(() => {
+        localStorage.setItem('polygonPaths', JSON.stringify(polygonPaths));
+    }, [polygonPaths]);
+
+  
+    // Event handlers for DrawingManager
+    const onLoad = drawingManager => {
+      console.log(drawingManager)
+    }
+
+  
+    const onPolygonComplete = polygon => {
+      const paths = polygon.getPath().getArray().map(latLng => {
+        return { lat: latLng.lat(), lng: latLng.lng() }
+      });
+      setPolygonPaths(paths);
+    }
+  
+    // Render the GoogleMap component with a Marker and DrawingManager
+    return (
+      <>
+        <div className="places-container">
+          <PlacesAutocomplete setSelected={setSelected} />
+        </div>
+      
+        <GoogleMap
+          id="drawing-manager-example"
+          mapContainerClassName="map-container"
+          zoom={15}
+          center={selected}
+        >
+          {selected && <Marker position={selected} />}
+          
+          <DrawingManager
+            onLoad={onLoad}
+            defaultDrawingMode={google.maps.drawing.OverlayType.POLYGON}
+            defaultOptions={{
+              drawingControl: true,
+              drawingControlOptions: {
+                position: google.maps.ControlPosition.TOP_CENTER,
+                drawingModes: [google.maps.drawing.OverlayType.POLYGON]
+              },
+              polygonOptions: { editable: true, draggable: true }
+            }}
+            onPolygonComplete={onPolygonComplete}
+          />
+  
+          {polygonPaths.length > 0 && (
+            <Polygon
+              paths={polygonPaths}
+              options={{ strokeColor: "#FF0000", fillColor: "#FF0000" }}
+            />
+          )}
+        </GoogleMap>
+      </>
+    );
+  }
 
 // Component for the PlacesAutocomplete search bar
 const PlacesAutocomplete = ({ setSelected }) => {
